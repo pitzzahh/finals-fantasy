a09b6db849d3a644b325f67a0d3cbaa12b629fd7
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 23:31:14 +0800

    :recycle: Refactor main entry point

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-import java.util.Locale;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         GameSettings gameSettings = new GameSettings();
-        if (args != null) {
-            for (String arg : args) {
-                gameSettings.clearConsole = arg.toLowerCase(Locale.ROOT).matches(".*clear.*");
-                gameSettings.colored = arg.toLowerCase(Locale.ROOT).matches(".*color.*");
-            }
-        }
+
+        gameSettings.colored = Arrays.stream(args).anyMatch(arg -> arg.matches("colored"));
+        gameSettings.gameFreeze = Arrays.stream(args)
+                .filter(arg -> arg.contains("game-freeze"))
+                .findAny()
+                .map(arg -> Long.parseLong(arg.split("=")[1]))
+                .orElse(4000L);
 
         Game game = new Game(gameSettings, new GameAssets(gameSettings));
         game.start(new Scanner(System.in));
