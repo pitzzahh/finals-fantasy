b30ebeafdc0d10202373dbc26bc29eca179fc927
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 12:26:10 +0800

    :bug: Fix coloring

--- src/Game.java
+++ src/Game.java
@@ -1,73 +1,102 @@
 import java.util.Scanner;
 
-
 public class Game extends GameSettings {
+
+    public Game() {
+        super();
+    }
 
     public Game(GameSettings gameSettings) {
         super(gameSettings);
     }
 
     protected void start(Scanner scanner) {
-        printAnimatedText(super.welcomeText, 0);
-        printAnimatedText(super.gameTitle, 0);
+        assets.printAnimatedText(super.assets.welcomeText(), 0);
+        assets.printAnimatedText(super.assets.gameTitle(), 0);
         while (!exit) {
             System.out.printf("SELECT %sGAME%s MODE%s\n",
-                    Color.PURPLE.value(),
-                    Color.GREEN.value(),
-                    Color.RESET.value()
+                    assets.colored(Color.PURPLE),
+                    assets.colored(Color.GREEN),
+                    assets.colored(Color.RESET)
             );
             for (GameMode mode : GameMode.values()) {
                 System.out.printf("%s: %s%s %s: %s%s%s\n",
-                        Color.GREEN.value(),
-                        Color.PURPLE.value(),
+                        assets.colored(Color.GREEN),
+                        assets.colored(Color.PURPLE),
                         mode.ordinal() + 1,
-                        Color.GREEN.value(),
-                        Color.BLUE.value(),
+                        assets.colored(Color.GREEN),
+                        assets.colored(Color.BLUE),
                         mode.name(),
-                        Color.RESET.value()
+                        assets.colored(Color.RESET)
                 );
             }
             System.out.printf("%s: %s%s %s: %s%s%s\n",
-                    Color.GREEN.value(),
-                    Color.PURPLE.value(),
+                    assets.colored(Color.GREEN),
+                    assets.colored(Color.PURPLE),
                     GameMode.values().length + 1,
-                    Color.GREEN.value(),
+                    assets.colored(Color.GREEN),
                     Color.RED.value(),
                     "Exit",
-                    Color.RESET.value()
+                    assets.colored(Color.RESET)
             );
-            System.out.printf("%s>>>:%s ", Color.YELLOW.value(), Color.RESET.value());
-            switch (scanner.next()) {
+            System.out.printf("%s>>>:%s ", Color.YELLOW.value(), assets.colored(Color.RESET));
+            switch (scanner.nextLine()) {
                 case "1":
-                    gameMode = GameMode.SINGLE_PLAYER;
-                    playerOne = new Player(
-                            generateHeroName(super.heroNames, super.heroNicknames, random),
-                            super.generateRandomColor(random)
-                    );
-                    playerTwo = new Player(
-                            generateHeroName(super.heroNames, super.heroNicknames, random),
-                            super.generateRandomColor(random)
-                    );
+                    singlePlayer(scanner);
                     break;
                 case "2":
-                    gameMode = GameMode.MULTI_PLAYER;
+                    multiPlayer(scanner);
                     break;
                 case "3":
-                    gameMode = GameMode.COMPUTER_ONLY;
+                    computerOnly();
                     break;
                 case "4":
-                    System.out.println("Exit");
                     exit = true;
                     scanner.close();
                     break;
                 default:
-                    System.out.println("Invalid input");
+                    System.out.printf("%sInvalid input%s\n", assets.colored(Color.RED_BOLD), assets.colored(Color.RESET));
                     break;
             }
         }
     }
 
-    /*
+    void singlePlayer(Scanner scanner) {
+        gameMode = GameMode.SINGLE_PLAYER;
 
-     */
+        // hero name for player
+        System.out.printf("%sEnter Hero 1 name: %s", assets.colored(Color.YELLOW), assets.colored(Color.RESET));
+        String name = scanner.nextLine();
+        playerOne = new Player(name, assets.generateRandomColor(random));
+        // hero name for computer
+        playerTwo = new Player(
+                generateHeroName(super.assets.heroNames, super.assets.heroNicknames, random),
+                super.assets.generateRandomColor(random)
+        );
+    }
+
+    void multiPlayer(Scanner scanner) {
+        gameMode = GameMode.MULTI_PLAYER;
+        System.out.printf("%sEnter Hero 1 name: %s", assets.colored(Color.YELLOW), assets.colored(Color.RESET));
+        String hero1 = scanner.nextLine();
+        playerOne = new Player(hero1, assets.generateRandomColor(random));
+
+        System.out.printf("%sEnter Hero 2 name: %s", assets.colored(Color.YELLOW), assets.colored(Color.RESET));
+        String hero2 = scanner.nextLine();
+        playerTwo = new Player(hero2, assets.generateRandomColor(random));
+    }
+
+    void computerOnly() {
+        gameMode = GameMode.COMPUTER_ONLY;
+        playerOne = new Player(
+                generateHeroName(super.assets.heroNames, super.assets.heroNicknames, random),
+                super.assets.generateRandomColor(random)
+        );
+
+        playerTwo = new Player(
+                generateHeroName(super.assets.heroNames, super.assets.heroNicknames, random),
+                super.assets.generateRandomColor(random)
+        );
+    }
+
 }
