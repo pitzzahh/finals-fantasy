4f1c64d143f25af3fd88c60aa5e414fc1d1ff1cf
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 12:29:42 +0800

    :sparkles: Implemented attack and defend actions

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,17 @@
-public class Player {
+import java.util.Objects;
+import java.util.Random;
+
+public class Player implements Action {
     private String name;
-    private String color;
+    private Color color;
     private int score;
     private int lives;
+    private int damage;
 
-    public Player(String name, String color) {
+    public Player(String name, Color color) {
         this.name = name;
         this.color = color;
-    }
-
-    public Player(String name, String color, int score, int lives) {
-        this.name = name;
-        this.color = color;
-        this.score = score;
-        this.lives = lives;
+        setLives(100);
     }
 
     public String getName() {
@@ -24,11 +22,11 @@ public class Player {
         this.name = name;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
-    public void setColor(String color) {
+    public void setColor(Color color) {
         this.color = color;
     }
 
@@ -46,5 +44,39 @@ public class Player {
 
     public void setLives(int lives) {
         this.lives = lives;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public Player attack(Player player) {
+        setDamage(new Random().nextInt(player.getLives() - (player.getLives() / 2)));
+        player.setLives(player.getLives() - getDamage());
+        return player.defend();
+    }
+
+    @Override
+    public Player defend() {
+        setDamage(0);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return getScore() == player.getScore() && getLives() == player.getLives() && Objects.equals(getName(), player.getName()) && Objects.equals(getColor(), player.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getColor(), getScore(), getLives());
     }
 }
