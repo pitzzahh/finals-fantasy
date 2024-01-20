aa3ae84dc6ed2d9a7952a207614d54ecca3c7e80
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 23:28:35 +0800

    :recycle: Refactor methods to be void because I said so

--- src/Action.java
+++ src/Action.java
@@ -2,13 +2,11 @@ public interface Action {
     /**
      * A player can attack another player
      * @param player The player to gain damage
-     * @return The player with the damage
      */
-    Player attack(Player player);
+    void attack(Player player);
 
     /**
      * A player can defend
-     * @return The player with the defense
      */
-    Player defend();
+    void defend();
 }
