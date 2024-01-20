fed2a806e59a6638039dda874b2226d790205c1b
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 23:29:00 +0800

    :recycle: Refactor player class implementations

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,8 @@
-import java.util.Objects;
 import java.util.Random;
 
 public class Player implements Action {
-    private String name;
-    private Color color;
-    private int score;
+    private final String name;
+    private final Color color;
     private int lives;
     private int damage;
 
@@ -18,24 +16,8 @@ public class Player implements Action {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Color getColor() {
         return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
     }
 
     public int getLives() {
@@ -55,28 +37,13 @@ public class Player implements Action {
     }
 
     @Override
-    public Player attack(Player player) {
-        setDamage(new Random().nextInt(player.getLives() - (player.getLives() / 2)));
+    public void attack(Player player) {
+        setDamage(new Random().nextInt(player.getLives()) + 1);
         player.setLives(player.getLives() - getDamage());
-        return player.defend();
     }
 
     @Override
-    public Player defend() {
+    public void defend() {
         setDamage(0);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return getScore() == player.getScore() && getLives() == player.getLives() && Objects.equals(getName(), player.getName()) && Objects.equals(getColor(), player.getColor());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getColor(), getScore(), getLives());
     }
 }
