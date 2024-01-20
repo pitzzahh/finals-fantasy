272e9bee197621e92c12a97fbf0c9496455c63a3
Peter John Arao <pitzzahh@gmail.com>
2024-01-20 10:48:43 +0800

    :recycle: Refactor Colors.java class to Color.class enum

---
+++ src/Color.java
@@ -0,0 +1,93 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 pitzzahh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+public enum Color {
+    RESET("\033[0m"),
+    BLACK("\033[0;30m"),
+    RED("\033[0;31m"),
+    GREEN("\033[0;32m"),
+    YELLOW("\033[0;33m"),
+    BLUE("\033[0;34m"),
+    PURPLE("\033[0;35m"),
+    CYAN("\033[0;36m"),
+    WHITE("\033[0;37m"),
+    BLACK_BOLD("\033[1;30m"),
+    RED_BOLD("\033[1;31m"),
+    GREEN_BOLD("\033[1;32m"),
+    YELLOW_BOLD("\033[1;33m"),
+    BLUE_BOLD("\033[1;34m"),
+    PURPLE_BOLD("\033[1;35m"),
+    CYAN_BOLD("\033[1;36m"),
+    WHITE_BOLD("\033[1;37m"),
+    BLACK_UNDERLINED("\033[4;30m"),
+    RED_UNDERLINED("\033[4;31m"),
+    GREEN_UNDERLINED("\033[4;32m"),
+    YELLOW_UNDERLINED("\033[4;33m"),
+    BLUE_UNDERLINED("\033[4;34m"),
+    PURPLE_UNDERLINED("\033[4;35m"),
+    CYAN_UNDERLINED("\033[4;36m"),
+    WHITE_UNDERLINED("\033[4;37m"),
+    BLACK_BACKGROUND("\033[40m"),
+    RED_BACKGROUND("\033[41m"),
+    GREEN_BACKGROUND("\033[42m"),
+    YELLOW_BACKGROUND("\033[43m"),
+    BLUE_BACKGROUND("\033[44m"),
+    PURPLE_BACKGROUND("\033[45m"),
+    CYAN_BACKGROUND("\033[46m"),
+    WHITE_BACKGROUND("\033[47m"),
+    BLACK_BRIGHT("\033[0;90m"),
+    RED_BRIGHT("\033[0;91m"),
+    GREEN_BRIGHT("\033[0;92m"),
+    YELLOW_BRIGHT("\033[0;93m"),
+    BLUE_BRIGHT("\033[0;94m"),
+    PURPLE_BRIGHT("\033[0;95m"),
+    CYAN_BRIGHT("\033[0;96m"),
+    WHITE_BRIGHT("\033[0;97m"),
+    BLACK_BOLD_BRIGHT("\033[1;90m"),
+    RED_BOLD_BRIGHT("\033[1;91m"),
+    GREEN_BOLD_BRIGHT("\033[1;92m"),
+    YELLOW_BOLD_BRIGHT("\033[1;93m"),
+    BLUE_BOLD_BRIGHT("\033[1;94m"),
+    PURPLE_BOLD_BRIGHT("\033[1;95m"),
+    CYAN_BOLD_BRIGHT("\033[1;96m"),
+    WHITE_BOLD_BRIGHT("\033[1;97m"),
+    BLACK_BACKGROUND_BRIGHT("\033[0;100m"),
+    RED_BACKGROUND_BRIGHT("\033[0;101m"),
+    GREEN_BACKGROUND_BRIGHT("\033[0;102m"),
+    YELLOW_BACKGROUND_BRIGHT("\033[0;103m"),
+    BLUE_BACKGROUND_BRIGHT("\033[0;104m"),
+    PURPLE_BACKGROUND_BRIGHT("\033[0;105m"),
+    CYAN_BACKGROUND_BRIGHT("\033[0;106m"),
+    WHITE_BACKGROUND_BRIGHT("\033[0;107m");
+
+    private final String value;
+
+    Color(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
